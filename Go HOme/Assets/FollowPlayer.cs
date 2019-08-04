ea732d538d5d0eb3c0d39122fa96ada6734897a7
LALJones <53327010+LALJones@users.noreply.github.com>
2019-08-03 18:57:06 -0600

    Fixed To Correct Project...

---
+++ Go HOme/Assets/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+
+    //Need referamce to the player (transform component is another datatype pretty much) it comes from the component fromthe squareee!)
+    //To point which objects transform to refer to just drag the plauer into it inside inspector lylylylullulu
+    //IF we WRITE TRANSFORM WITH  A LOWER CASE T it actually refers to the trasnform of the object that we are curretnly on :)
+    public Transform player;
+    public Vector3 offset;                              //Vector 3 is another data type like transform. A vector stores 3 floats. Therefor, its optimal to se for postions (since we have a x y and z axis.)
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + offset;
+
+    }
+}
---
+++ Go HOme/Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    void FixedUpdate()
+    {
+        //Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+            //Only executed if the condition is met :>
+        }
+        if(Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        } //FixedUpdate is always slower so beaware and cn be missed.
+    }
+}
